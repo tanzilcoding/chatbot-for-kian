0252798238fa4b9d762bdbdfec41ae9c56247cec
tanzilcoding <89991847+tanzilcoding@users.noreply.github.com>
2023-07-14 00:39:41 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,7 @@ try:
     pinecone_environment = os.environ['pinecone_environment']
     openai.organization = os.environ['openai_organization']
     openai.api_key = os.environ['openai_api_key']
-    # OPENAI_API_KEY = os.environ['openai_api_key']
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = os.environ['openai_api_key']
 
     # st.text(pinecone_api_key)
     # st.text(pinecone_environment)
