96914ccc1e1c226280a6dcd6fbca9f614767300a
tanzilcoding <89991847+tanzilcoding@users.noreply.github.com>
2023-07-14 00:24:28 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,4 +184,4 @@ except Exception as e:
         error_message = e.message
     else:
         error_message = e
-    # st.error('ERROR MESSAGE: {}'.format(error_message))
+    st.error('ERROR MESSAGE: {}'.format(error_message))
