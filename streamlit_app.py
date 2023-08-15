2283d582a956d7fd6e41294b9d213750a88abb66
tanzilcoding <89991847+tanzilcoding@users.noreply.github.com>
2023-08-15 12:44:06 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import time
+import traceback
 import openai
 import pinecone
 import streamlit as st
@@ -7,8 +9,8 @@ from streamlit_chat import message
 from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
 try:
     import environment_variables
@@ -21,47 +23,85 @@ try:
     st.markdown("<h1 style='text-align: center;'>AI ChatBot 😬</h1>",
                 unsafe_allow_html=True)
 
-    # Get environment variables
-    # openai.organization = os.environ['openai_organization']
-    # =======================================================
+    # Step 1: Get common environment variables
     OPENAI_API_KEY = os.environ['openai_api_key']
-    pinecone_api_key = os.environ['pinecone_api_key']
-    pinecone_environment = os.environ['pinecone_environment']
     openai.api_key = OPENAI_API_KEY
     os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-    index_name = os.environ['index_name']
-    # ==================================================== #
 
-    # initialize connection to pinecone (get API key at app.pinecone.io)
-    pinecone.init(
-        api_key=pinecone_api_key,
-        environment=pinecone_environment  # find next to API key in console
-    )
-
-    # connect to index
-    index = pinecone.Index(index_name)
-    # wait a moment for the index to be fully initialized
-    time.sleep(1)
-    # stats = index.describe_index_stats()
-
-    # get openai api key from platform.openai.com
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') or 'OPENAI_API_KEY'
-
+    text_field = "text"
     model_name = 'text-embedding-ada-002'
-
     embed = OpenAIEmbeddings(
         model=model_name,
         openai_api_key=OPENAI_API_KEY
     )
 
-    text_field = "text"
+    # Step 2: Get Pinecone.io database specific environment variables
 
-    # switch back to normal index for langchain
-    index = pinecone.Index(index_name)
+    # Databaes 1: Problem Statement Pinecone.io Database
+    # =======================================================
+    problem_statement_pinecone_api_key = os.environ['problem_statement_pinecone_api_key']
+    problem_statement_pinecone_environment = os.environ['problem_statement_pinecone_environment']
+    problem_statement_index_name = os.environ['problem_statement_index_name']
 
-    vectorstore = Pinecone(
-        index, embed.embed_query, text_field
+    # Initialize connection to pinecone (get API key at app.pinecone.io)
+    pinecone.init(
+        api_key=problem_statement_pinecone_api_key,
+        environment=problem_statement_pinecone_environment
     )
+
+    # Connect to the index
+    problem_statement_index = pinecone.Index(problem_statement_index_name)
+    # Wait a moment for the index to be fully initialized
+    time.sleep(1)
+
+    problem_statement_vectorstore = Pinecone(
+        problem_statement_index, embed.embed_query, text_field
+    )
+    # ==================================================== #
+
+    # Databaes 2: CHILD Pinecone.io Database
+    # =======================================================
+    child_pinecone_api_key = os.environ['child_pinecone_api_key']
+    child_pinecone_environment = os.environ['child_pinecone_environment']
+    child_index_name = os.environ['child_index_name']
+
+    # Initialize connection to pinecone (get API key at app.pinecone.io)
+    pinecone.init(
+        api_key=child_pinecone_api_key,
+        environment=child_pinecone_environment  # find next to API key in console
+    )
+
+    # Connect to the index
+    child_index = pinecone.Index(child_index_name)
+    # Wait a moment for the index to be fully initialized
+    time.sleep(1)
+
+    child_vectorstore = Pinecone(
+        child_index, embed.embed_query, text_field
+    )
+    # ==================================================== #
+
+    # Databaes 3: Market Solutions Pinecone.io Database
+    # =======================================================
+    market_solutions_pinecone_api_key = os.environ['market_solutions_pinecone_api_key']
+    market_solutions_pinecone_environment = os.environ['market_solutions_pinecone_environment']
+    market_solutions_index_name = os.environ['market_solutions_index_name']
+
+    # Initialize connection to pinecone (get API key at app.pinecone.io)
+    pinecone.init(
+        api_key=market_solutions_pinecone_api_key,
+        environment=market_solutions_pinecone_environment
+    )
+
+    # Connect to the index
+    market_solutions_index = pinecone.Index(market_solutions_index_name)
+    # Wait a moment for the index to be fully initialized
+    time.sleep(1)
+
+    market_solutions_vectorstore = Pinecone(
+        market_solutions_index, embed.embed_query, text_field
+    )
+    # ==================================================== #
 
     # Initialise session state variables
     if 'generated' not in st.session_state:
@@ -76,6 +116,8 @@ try:
         st.session_state['model_name'] = []
     if 'problem_statement_list' not in st.session_state:
         st.session_state['problem_statement_list'] = []
+    if 'child_response' not in st.session_state:
+        st.session_state['child_response'] = []
 
     # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
     st.sidebar.title("Sidebar")
@@ -89,6 +131,13 @@ try:
         model = "gpt-3.5-turbo-16k"
     else:
         model = "gpt-4"
+
+    # Initialize the large language model
+    llm = ChatOpenAI(
+        temperature=0,
+        openai_api_key=OPENAI_API_KEY,
+        model_name=model,
+    )
 
     # reset everything
     if clear_button:
@@ -117,7 +166,8 @@ try:
         # for doc in docs:
         #     st.sidebar.text(doc.metadata['problem_statement'])
 
-        docs_and_scores = vectorstore.similarity_search_with_score(query)
+        docs_and_scores = problem_statement_vectorstore.similarity_search_with_score(
+            query)
 
         raw_problem_statement_list = []
         problem_statement_list = []
@@ -146,45 +196,49 @@ try:
                     problem_statement_list.append(
                         {"score": score, "problem_statement": problem_statement, "year": year, "requestor": requestor, })
 
-        # completion llm
-        llm = ChatOpenAI(
-            openai_api_key=OPENAI_API_KEY,
-            model_name=model,
-            temperature=0.0
-        )
-
-        # system message to 'prime' the model
-        primer = f"""You are a Q&A bot. A highly intelligent system that answers
-                user questions based on the information provided by the user above
-                each question. If the information can not be found in the information
-                provided by the user you truthfully say "I don't know".
-                """
-
-        messages = [
-            SystemMessage(
-                content=primer
-            ),
-            HumanMessage(
-                content=query
-            ),
-        ]
-
-        llm(messages)
-
-        qa = RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type="stuff",
-            retriever=vectorstore.as_retriever()
-        )
-
-        response = qa.run(query)
-        response = response.strip()
+        max_token_limit = 4096
+        # Problem Statement
         ######################################################
-        # response = ""
-        st.session_state['messages'].append(
-            {"role": "assistant", "content": response})
+        problem_statement_memory = ConversationSummaryBufferMemory(
+            llm=llm,
+            memory_key="chat_history",
+            return_messages=True,
+            max_token_limit=max_token_limit,
+        )
 
-        return response, problem_statement_list
+        chain = ConversationalRetrievalChain.from_llm(
+            llm,
+            retriever=problem_statement_vectorstore.as_retriever(),
+            # retriever=problem_statement_vectorstore.as_retriever(search_kwargs={"k": 3}),
+            memory=problem_statement_memory,
+        )
+        response = chain.run({'question': query})
+        problem_statement_response = response.strip()
+        ######################################################
+
+        # Problem Statement
+        ######################################################
+        child_memory = ConversationSummaryBufferMemory(
+            llm=llm,
+            memory_key="chat_history",
+            return_messages=True,
+            max_token_limit=max_token_limit,
+        )
+
+        chain = ConversationalRetrievalChain.from_llm(
+            llm,
+            retriever=child_vectorstore.as_retriever(),
+            # retriever=problem_statement_vectorstore.as_retriever(search_kwargs={"k": 3}),
+            memory=child_memory,
+        )
+        response = chain.run({'question': query})
+        child_response = response.strip()
+        ######################################################
+
+        st.session_state['messages'].append(
+            {"role": "assistant", "content": problem_statement})
+
+        return problem_statement_response, problem_statement_list, child_response
 
     # container for chat history
     response_container = st.container()
@@ -197,13 +251,14 @@ try:
             submit_button = st.form_submit_button(label='Send')
 
         if submit_button and user_input:
-            output, problem_statement_list = generate_response(
+            problem_statement_response, problem_statement_list, child_response = generate_response(
                 user_input)
             st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
+            st.session_state['generated'].append(problem_statement_response)
             st.session_state['model_name'].append(model_name)
             st.session_state['problem_statement_list'].append(
                 problem_statement_list)
+            st.session_state['child_response'].append(child_response)
 
     if st.session_state['generated']:
         with response_container:
@@ -245,8 +300,11 @@ try:
                         st.markdown(
                             f"""<span style="word-wrap:break-word;"><strong>Requestor/Dept/Institution:</strong><br>{requestor}""", unsafe_allow_html=True)
 
+                generated_answer = st.session_state["generated"][i]
+                st.markdown(
+                    f"""<span style="word-wrap:break-word;"><strong>Summary:</strong><br>{generated_answer}""", unsafe_allow_html=True)
                 message(
-                    f'Summary: {st.session_state["generated"][i]}', key=str(i))
+                    f'Answer from the CHILD Database: {st.session_state["child_response"][i]}', key=str(i))
 
 
 except Exception as e:
@@ -259,4 +317,10 @@ except Exception as e:
         error_message = e.message
     else:
         error_message = e
-    st.error('ERROR MESSAGE: {}'.format(error_message))
+    st.error('ERROR MESSAGE: {}'.format(error_message), icon="🚨")
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    # st.error(f'Error Type: {exc_type}', icon="🚨")
+    # st.error(f'File Name: {fname}', icon="🚨")
+    # st.error(f'Line Number: {exc_tb.tb_lineno}', icon="🚨")
+    # print(traceback.format_exc())
